c4dc3de3da790acf3828f31d393cc552334a6c4b
SANTIAGO RAMÍREZ FORERO <santiforero57@gmail.com>
2024-01-03 09:12:36 -0500

    navbar responsive

--- components/ButtonPrimary.js
+++
@@ -1,8 +0,0 @@
-import { Button } from "@nextui-org/react";
-import React from "react";
-
-const ButtonPrimary = ({ text }) => {
-  return <Button className="bg-[#485098] font-bold text-white">{text}</Button>;
-};
-
-export default ButtonPrimary;
--- components/NewNav.js
+++ components/NewNav.js
@@ -6,14 +6,35 @@ import {
   NavbarItem,
   Link,
   Button,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
 } from "@nextui-org/react";
 
 import { AcmeLogo } from "./AcmeLogo.jsx";
 
 export function NewNav() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "",
+    "Inicio",
+    "Acerca de",
+    "Proyectos",
+    "Testimonios",
+    "Equipo",
+  ];
   return (
-    <Navbar className="pt-5 w-full " maxWidth="2xl">
+    <Navbar
+      className="pt-5 w-full "
+      maxWidth="2xl"
+      onMenuOpenChange={setIsMenuOpen}
+    >
       <NavbarBrand>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
         <AcmeLogo />
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4 " justify="center">
@@ -46,6 +67,26 @@ export function NewNav() {
           </Button>
         </NavbarItem>
       </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2
+                  ? "primary"
+                  : index === menuItems.length - 1
+                  ? "foreground"
+                  : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   );
 }
