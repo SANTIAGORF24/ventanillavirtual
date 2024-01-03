5c029c7995757d31de5c89036988e6be9a3b1b7f
SANTIAGO RAMÍREZ FORERO <santiforero57@gmail.com>
2024-01-03 10:20:34 -0500

    works y en proceso team

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import { NewAbout } from "@/components/Home/AboutUs/NewAbout";
 import NewContactBanner from "@/components/Home/NewContactBanner";
 import NewBanner from "@/components/Home/NewBanner";
 import NewProyects from "@/components/Home/Proyects/NewProyects";
+import Works from "@/components/Home/Proyects/Works";
+import Team from "@/components/Home/Team/Team";
 export default function Home() {
   return (
     <>
@@ -9,6 +11,8 @@ export default function Home() {
       <NewContactBanner />
       <NewAbout />
       <NewProyects />
+      <Works />
+      <Team />
     </>
   );
 }
