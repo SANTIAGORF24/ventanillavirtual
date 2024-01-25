38bce430d6bac691c3e0579530f124cf138a38cb
JohanSora <johan.sora@mymarketlogic.com>
2024-01-24 22:47:24 -0500

    add changes

--- components/Home/Team/Team.js
+++ components/Home/Team/Team.js
@@ -31,20 +31,31 @@ const Team = () => {
           modules={[Autoplay, Pagination, Navigation]}
           className="w-full h-full"
         >
-          <SwiperSlide className="text-center text-base bg-white flex justify-center items-center">
+          <SwiperSlide className="text-center text-base bg-white flex justify-center items-center px-12">
             <div>
               <Card className="p-5 flex items-center justify-center ">
                 <div className="sm:flex items-center max-w-8xl">
                   <div className="flex justify-center">
                     <img
-                      className="max-sm:w-3/4 rounded-md"
+                      className="max-sm:w-3/4 rounded-md max-w-[200px]"
                       src="./assets/img/portada.jpg"
                     />
                   </div>
                   <div className="flex-colum max-sm:py-3">
                     <div>
                       <p>
-                      Soy Santiago Ramirez Forero. Estudiante de Ingeniería de Sistemas con experiencia estratégica en el ámbito gubernamental y nacional, destacándome en proyectos con HTML, CSS3, Python, MySQL, Flutter, Java, React, Nextjs y Tailwind. Como Especialista en Soporte de Aplicaciones en el Ministerio de Deporte, lideré el soporte técnico de GESDOC, implementando funciones y garantizando eficiencia. Combino habilidades en soporte técnico y diseño multimedia para contribuir al éxito operativo y creativo en entornos profesionales. Busco nuevos desafíos para seguir creciendo en la ingeniería de sistemas.
+                        Soy Santiago Ramirez Forero. Estudiante de Ingeniería de
+                        Sistemas con experiencia estratégica en el ámbito
+                        gubernamental y nacional, destacándome en proyectos con
+                        HTML, CSS3, Python, MySQL, Flutter, Java, React, Nextjs
+                        y Tailwind. Como Especialista en Soporte de Aplicaciones
+                        en el Ministerio de Deporte, lideré el soporte técnico
+                        de GESDOC, implementando funciones y garantizando
+                        eficiencia. Combino habilidades en soporte técnico y
+                        diseño multimedia para contribuir al éxito operativo y
+                        creativo en entornos profesionales. Busco nuevos
+                        desafíos para seguir creciendo en la ingeniería de
+                        sistemas.
                       </p>
                     </div>
                     <div className="sm:flex justify-between items-center p-10">
@@ -100,20 +111,29 @@ const Team = () => {
               </Card>
             </div>
           </SwiperSlide>
-          <SwiperSlide className="text-center text-base bg-white flex justify-center items-center">
+          <SwiperSlide className="text-center text-base bg-white flex justify-center items-center px-12">
             <div>
               <Card className="p-5 flex items-center justify-center ">
                 <div className="sm:flex items-center max-w-8xl">
                   <div className="flex justify-center items-center">
                     <img
-                      className="max-sm:w-3/4 rounded-md"
+                      className="max-sm:w-3/4 rounded-md max-w-[200px]"
                       src="assets/img/Foto_Mac.jpeg"
                     />
                   </div>
                   <div className="flex-colum max-sm:py-3">
                     <div>
                       <p>
-                      Soy Macjainer Molano Ramos, estudiante de Ingeniería de Sistemas, apasionado por la ingeniería de software y desarrollo web. Con sólida formación académica, destaco en diseño e implementación de sistemas informáticos, así como en desarrollo Front End (HTML, CSS, JavaScript, React.js) y bases de datos (SQL, MySQL, Python). Mi experiencia en servicio al cliente fortaleció habilidades comunicativas, pero mi enfoque profesional se centra en la programación. Activamente en busca de oportunidades para contribuir a proyectos desafiantes
+                        Soy Macjainer Molano Ramos, estudiante de Ingeniería de
+                        Sistemas, apasionado por la ingeniería de software y
+                        desarrollo web. Con sólida formación académica, destaco
+                        en diseño e implementación de sistemas informáticos, así
+                        como en desarrollo Front End (HTML, CSS, JavaScript,
+                        React.js) y bases de datos (SQL, MySQL, Python). Mi
+                        experiencia en servicio al cliente fortaleció
+                        habilidades comunicativas, pero mi enfoque profesional
+                        se centra en la programación. Activamente en busca de
+                        oportunidades para contribuir a proyectos desafiantes
                       </p>
                     </div>
                     <div className="sm:flex justify-between items-center p-10">
@@ -169,20 +189,28 @@ const Team = () => {
               </Card>
             </div>
           </SwiperSlide>
-          <SwiperSlide className="text-center text-base bg-white flex justify-center items-center">
+          <SwiperSlide className="text-center text-base bg-white flex justify-center items-center px-12">
             <div>
               <Card className="p-5 flex items-center justify-center ">
                 <div className="sm:flex items-center max-w-8xl">
                   <div className="flex justify-center items-center">
                     <img
-                      className="max-sm:w-3/4 rounded-md"
+                      className="max-sm:w-3/4 rounded-md max-w-[200px]"
                       src="assets/img/Foto_Sebastian.jfif"
                     />
                   </div>
                   <div className="flex-colum max-sm:py-3">
                     <div>
                       <p>
-                      Soy Johan Sora, desarrollador de front-end con 3 años de experiencia en ReactJs, Javascript, HTML, CSS, SASS, Redux y NodeJs. Desde los 12 años, mi pasión por la tecnología me ha llevado a aprender constantemente. Comencé con cursos en línea y un Bootcamp para fortalecer mis habilidades. Además del desarrollo, he adquirido conocimientos en marketing, destacándome en proyectos con habilidades en React Native, Mongo DB, Express.js, Django y Python.
+                        Soy Johan Sora, desarrollador de front-end con 3 años de
+                        experiencia en ReactJs, Javascript, HTML, CSS, SASS,
+                        Redux y NodeJs. Desde los 12 años, mi pasión por la
+                        tecnología me ha llevado a aprender constantemente.
+                        Comencé con cursos en línea y un Bootcamp para
+                        fortalecer mis habilidades. Además del desarrollo, he
+                        adquirido conocimientos en marketing, destacándome en
+                        proyectos con habilidades en React Native, Mongo DB,
+                        Express.js, Django y Python.
                       </p>
                     </div>
                     <div className="sm:flex justify-between items-center p-10">
@@ -238,20 +266,32 @@ const Team = () => {
               </Card>
             </div>
           </SwiperSlide>
-          <SwiperSlide className="text-center text-base bg-white flex justify-center items-center">
+          <SwiperSlide className="text-center text-base bg-white flex justify-center items-center px-12">
             <div>
               <Card className="p-5 flex items-center justify-center ">
                 <div className="sm:flex items-center max-w-8xl">
                   <div className="flex justify-center items-center">
                     <img
-                      className="max-sm:w-3/4 rounded-md"
+                      className="max-sm:w-3/4 rounded-md max-w-[200px]"
                       src="assets/img/Foto_Charles.jfif"
                     />
                   </div>
                   <div className="flex-colum max-sm:py-3">
                     <div>
                       <p>
-                      Soy Carlos Enrrique Vargas, un desarrollador web full stack con más de 6 años de experiencia en sitios web, aplicaciones y e-commerce. Mis habilidades abarcan HTML, CSS, JavaScript, PHP y bases de datos, con experiencia en frameworks como JavaScript, PHP y Python. He trabajado en plataformas de cloud como Google Cloud, AWS y Digital Ocean, y tengo experiencia en SEO técnico, integraciones de datos y diseño de frontend. Mi enfoque en estrategias de marketing digital y capacidad para adaptarme a situaciones de alta presión me hacen un activo valioso. Soy proactivo, orientado a metas y siempre listo para aprender y crecer, seguro de aportar valor a cualquier equipo o proyecto.
+                        Soy Carlos Enrrique Vargas, un desarrollador web full
+                        stack con más de 6 años de experiencia en sitios web,
+                        aplicaciones y e-commerce. Mis habilidades abarcan HTML,
+                        CSS, JavaScript, PHP y bases de datos, con experiencia
+                        en frameworks como JavaScript, PHP y Python. He
+                        trabajado en plataformas de cloud como Google Cloud, AWS
+                        y Digital Ocean, y tengo experiencia en SEO técnico,
+                        integraciones de datos y diseño de frontend. Mi enfoque
+                        en estrategias de marketing digital y capacidad para
+                        adaptarme a situaciones de alta presión me hacen un
+                        activo valioso. Soy proactivo, orientado a metas y
+                        siempre listo para aprender y crecer, seguro de aportar
+                        valor a cualquier equipo o proyecto.
                       </p>
                     </div>
                     <div className="sm:flex justify-between items-center p-10">
