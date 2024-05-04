ec815ed084fa8d6ab30d00775143ea4ecd2cb239
Santiago Ramirez Forero <santiforero57@gmail.com>
2024-05-04 18:24:21 -0500

    responsive xd

--- components/Home/Clientes/Clientes.js
+++ components/Home/Clientes/Clientes.js
@@ -1,98 +1,95 @@
 import React from "react";
-import { Tabs, Tab, Card, CardBody, Button, Image } from "@nextui-org/react";
 
 const Clientes = () => {
   return (
-    <>
-      <div className="w-full flex flex-col justify-center items-center">
-        <div className="w-4/6 flex flex-col justify-center items-center">
-          <div className="flex flex-col justify-center items-center space-y-8">
-            <p>500 personas han dicho que nos prefieren</p>
-            <h2 className="font-extrabold text-4xl w-4/6 text-center">
-              Nuestros clientes felices dicen sobre nosotros
-            </h2>
+    <div className="w-full flex flex-col justify-center items-center">
+      <div className="w-full max-w-4xl flex flex-col justify-center items-center px-4">
+        <div className="flex flex-col justify-center items-center space-y-8">
+          <p>500 personas han dicho que nos prefieren</p>
+          <h2 className="font-extrabold text-4xl md:text-5xl lg:text-6xl text-center">
+            Nuestros clientes felices dicen sobre nosotros
+          </h2>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center md:justify-between mt-8">
+          <div className="bg-blue-200 rounded-lg p-6 text-center w-full md:w-96 mx-4 mb-8 md:mb-0">
+            <div className="flex justify-center mb-4">
+              {[...Array(5)].map((_, index) => (
+                <svg
+                  key={index}
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-8 w-8 text-yellow-500 fill-current"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 15.333l4.472 2.682-1.083-5.93 3.472-3.327-5.833-.85L10 2.167l-2.028 5.739-5.833.85 3.472 3.327-1.083 5.93L10 15.333z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ))}
+            </div>
+            <p className="text-lg font-semibold">
+              Colegio Nacional De Curadores Urbanos
+            </p>
+            <p className="text-xs mb-2">
+              Excelente trabajo en la creación de nuestra landing en WordPress.
+              ¡Gracias por tu esfuerzo!
+            </p>
+            <p className="text-sm">Colegio Nacional De Curadores Urbanos</p>
           </div>
-          <div className="flex justify-between mt-8">
-            <div className="bg-blue-200 rounded-lg p-6 text-center w-2/3 mx-4">
-              <div className="flex justify-center mb-4">
-                {[...Array(5)].map((_, index) => (
-                  <svg
-                    key={index}
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-8 w-8 text-yellow-500 fill-current"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 15.333l4.472 2.682-1.083-5.93 3.472-3.327-5.833-.85L10 2.167l-2.028 5.739-5.833.85 3.472 3.327-1.083 5.93L10 15.333z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                ))}
-              </div>
-              <p className="text-lg font-semibold">
-                Colegio Nacional De Curadores Urbanos
-              </p>
-              <p className="text-xs mb-2">
-                Excelente trabajo en la creación de nuestra landing en
-                WordPress. ¡Gracias por tu esfuerzo!
-              </p>
-              <p className="text-sm">Colegio Nacional De Curadores Urbanos</p>
+          <div className="bg-blue-200 rounded-lg p-6 text-center w-full md:w-96 mx-4 mb-8 md:mb-0">
+            <div className="flex justify-center mb-4">
+              {[...Array(5)].map((_, index) => (
+                <svg
+                  key={index}
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-8 w-8 text-yellow-500 fill-current"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 15.333l4.472 2.682-1.083-5.93 3.472-3.327-5.833-.85L10 2.167l-2.028 5.739-5.833.85 3.472 3.327-1.083 5.93L10 15.333z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ))}
             </div>
-            <div className="bg-blue-200 rounded-lg p-6 text-center w-2/3 mx-4">
-              <div className="flex justify-center mb-4">
-                {[...Array(5)].map((_, index) => (
-                  <svg
-                    key={index}
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-8 w-8 text-yellow-500 fill-current"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 15.333l4.472 2.682-1.083-5.93 3.472-3.327-5.833-.85L10 2.167l-2.028 5.739-5.833.85 3.472 3.327-1.083 5.93L10 15.333z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                ))}
-              </div>
-              <p className="text-lg font-semibold">Flow-inc</p>
-              <p className="text-xs mb-2">
-                Estamos muy contentos con la landing que desarrollaste desde
-                cero. El código es limpio y eficiente.
-              </p>
-              <p className="text-sm">Flow-inc</p>
+            <p className="text-lg font-semibold">Flow-inc</p>
+            <p className="text-xs mb-2">
+              Estamos muy contentos con la landing que desarrollaste desde cero.
+              El código es limpio y eficiente.
+            </p>
+            <p className="text-sm">Flow-inc</p>
+          </div>
+          <div className="bg-blue-200 rounded-lg p-6 text-center w-full md:w-96 mx-4">
+            <div className="flex justify-center mb-4">
+              {[...Array(5)].map((_, index) => (
+                <svg
+                  key={index}
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-8 w-8 text-yellow-500 fill-current"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 15.333l4.472 2.682-1.083-5.93 3.472-3.327-5.833-.85L10 2.167l-2.028 5.739-5.833.85 3.472 3.327-1.083 5.93L10 15.333z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ))}
             </div>
-            <div className="bg-blue-200 rounded-lg p-6 text-center w-2/3 mx-4">
-              <div className="flex justify-center mb-4">
-                {[...Array(5)].map((_, index) => (
-                  <svg
-                    key={index}
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-8 w-8 text-yellow-500 fill-current"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 15.333l4.472 2.682-1.083-5.93 3.472-3.327-5.833-.85L10 2.167l-2.028 5.739-5.833.85 3.472 3.327-1.083 5.93L10 15.333z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                ))}
-              </div>
-              <p className="text-lg font-semibold">AppReview</p>
-              <p className="text-xs mb-2">
-                Estoy muy impresionado con el trabajo que realizaste en nuestro
-                proyecto de desarrollo personalizado. La forma en que pudiste
-                materializar nuestras ideas en código fue realmente
-                impresionante. Gracias por tu profesionalismo y dedicación.
-              </p>
-              <p className="text-sm">John Doe</p>
-            </div>
+            <p className="text-lg font-semibold">AppReview</p>
+            <p className="text-xs mb-2">
+              Estoy muy impresionado con el trabajo que realizaste en nuestro
+              proyecto de desarrollo personalizado. La forma en que pudiste
+              materializar nuestras ideas en código fue realmente impresionante.
+              Gracias por tu profesionalismo y dedicación.
+            </p>
+            <p className="text-sm">John Doe</p>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- components/Home/Proyects/NewProyects.js
+++ components/Home/Proyects/NewProyects.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { Tabs, Tab, Card, CardBody, Button, Image } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 
 const NewProyects = () => {
   return (
-    <>
-      <div className="mt-40 w-full flex flex-col justify-center items-center">
-        <div className="flex flex-col justify-center items-center space-y-8">
-          <h2 className="font-extrabold text-5xl w-4/6 text-center">
+    <div className="mt-20 w-full flex flex-col justify-center items-center px-4">
+      <div className="w-5/6 flex flex-col justify-center items-center">
+        <div className="mt-10 flex flex-col w-4/6 justify-center items-center space-y-8">
+          <h2 className="font-extrabold text-3xl md:text-5xl text-center">
             Toma la decisión acertada para tu negocio
           </h2>
-          <p>Elija Uno de nuestros productos asequibles</p>
+          <p>Elija uno de nuestros productos asequibles</p>
         </div>
-        <div className="flex justify-between w-5/6 py-20">
-          <div className="w-1/3 p-8 border border-blue-500 rounded-lg mr-4">
-            <h3 className="text-3xl font-bold mb-4">Landing en WordPress</h3>
-            <p className="text-gray-500 text-4xl mb-4">$400 USD</p>
+        <div className="flex flex-col md:flex-row justify-center items-center w-full py-10 space-y-10 md:space-y-0 md:space-x-10">
+          <div className="w-full md:w-1/3 p-6 md:p-8 border border-blue-500 rounded-lg mb-8 md:mb-0">
+            <h3 className="text-xl md:text-3xl font-bold mb-4">
+              Landing en WordPress
+            </h3>
+            <p className="text-gray-500 text-lg md:text-xl mb-4">$400 USD</p>
             <p className="mb-4">
               ¿Necesitas una presencia en línea efectiva y atractiva para tu
               negocio? Nuestro servicio de creación de landings en WordPress es
@@ -23,21 +25,21 @@ const NewProyects = () => {
               ayudaremos a destacar en la web y atraer a tus clientes
               potenciales.
             </p>
-            <Button className="bg-blue-200 text-gray-500 mb-4 w-full text-xl">
+            <Button className="bg-blue-200 text-gray-500 mb-4 w-full md:w-auto">
               Empezar
             </Button>
-            <h4 className="font-semibold mb-4">Qué incluye:</h4>
-            <ul>
-              <li className="mb-2">Desarrollo de diseño personalizado</li>
-              <li className="mb-2">Integración de plugins esenciales</li>
-              <li className="mb-2">Optimización para SEO</li>
+            <h4 className="font-semibold mb-2">Qué incluye:</h4>
+            <ul className="text-sm">
+              <li>Desarrollo de diseño personalizado</li>
+              <li>Integración de plugins esenciales</li>
+              <li>Optimización para SEO</li>
             </ul>
           </div>
-          <div className="w-1/3 p-8 border border-blue-500 rounded-lg mr-4 mt-[-40px]">
-            <h3 className="text-3xl font-bold mb-4">
+          <div className="w-full md:w-1/3 p-6 md:p-8 border border-blue-500 rounded-lg mb-8 md:mb-0">
+            <h3 className="text-xl md:text-3xl font-bold mb-4">
               Desarrollo de Aplicaciones Personalizado
             </h3>
-            <p className="text-gray-500 text-4xl mb-4">Cotizar</p>
+            <p className="text-gray-500 text-lg md:text-xl mb-4">Cotizar</p>
             <p className="mb-4">
               ¿Necesitas una aplicación que se adapte perfectamente a las
               necesidades específicas de tu negocio? Nuestro servicio de
@@ -46,23 +48,25 @@ const NewProyects = () => {
               aplicaciones robustas y funcionales que potencian tu negocio y
               proporcionan una experiencia óptima para tus usuarios.
             </p>
-            <Button className="bg-blue-200 text-gray-500 mb-4 w-full text-xl">
+            <Button className="bg-blue-200 text-gray-500 mb-4 w-full md:w-auto">
               Cotizar
             </Button>
-            <h4 className="font-semibold mb-4">Qué ofrecemos:</h4>
-            <ul>
-              <li className="mb-2">Análisis de requisitos y consultoría</li>
-              <li className="mb-2">
-                Diseño de interfaz de usuario personalizada
-              </li>
-              <li className="mb-2">Desarrollo de funcionalidades a medida</li>
-              <li className="mb-2">Pruebas de calidad y rendimiento</li>
-              <li className="mb-2">Soporte continuo y mantenimiento</li>
+            <h4 className="font-semibold mb-2">Qué ofrecemos:</h4>
+            <ul className="text-sm">
+              <li>Análisis de requisitos y consultoría</li>
+              <li>Diseño de interfaz de usuario personalizada</li>
+              <li>Desarrollo de funcionalidades a medida</li>
+              <li>Pruebas de calidad y rendimiento</li>
+              <li>Soporte continuo y mantenimiento</li>
             </ul>
           </div>
-          <div className="w-1/3 p-8 border border-blue-500 rounded-lg mr-4">
-            <h3 className="text-3xl font-bold mb-4">Landing a Código</h3>
-            <p className="text-gray-500 text-4xl mb-4">$600 - $1000 USD</p>
+          <div className="w-full md:w-1/3 p-6 md:p-8 border border-blue-500 rounded-lg mb-8 md:mb-0">
+            <h3 className="text-xl md:text-3xl font-bold mb-4">
+              Landing a Código
+            </h3>
+            <p className="text-gray-500 text-lg md:text-xl mb-4">
+              $600 - $1000 USD
+            </p>
             <p className="mb-4">
               ¿Buscas llevar tu presencia en línea al siguiente nivel con una
               landing totalmente personalizada y única? Nuestro servicio de
@@ -71,23 +75,21 @@ const NewProyects = () => {
               optimización para motores de búsqueda, te ayudaremos a destacar en
               la web y atraer a tus clientes potenciales.
             </p>
-            <Button className="bg-blue-200 text-gray-500 mb-4 w-full text-xl">
+            <Button className="bg-blue-200 text-gray-500 mb-4 w-full md:w-auto">
               Empezar
             </Button>
-            <h4 className="font-semibold mb-4">Qué ofrecemos:</h4>
-            <ul>
-              <li className="mb-2">Desarrollo de diseño personalizado</li>
-              <li className="mb-2">
-                Integración de funcionalidades específicas
-              </li>
-              <li className="mb-2">Optimización avanzada para SEO</li>
-              <li className="mb-2">Integración de sistemas de pago</li>
-              <li className="mb-2">Soporte técnico continuo</li>
+            <h4 className="font-semibold mb-2">Qué ofrecemos:</h4>
+            <ul className="text-sm">
+              <li>Desarrollo de diseño personalizado</li>
+              <li>Integración de funcionalidades específicas</li>
+              <li>Optimización avanzada para SEO</li>
+              <li>Integración de sistemas de pago</li>
+              <li>Soporte técnico continuo</li>
             </ul>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
