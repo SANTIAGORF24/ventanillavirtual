3f65c1e38cf7aeb5a012b06197c8e65ecad2c6b3
Santiago Ramirez Forero <santiforero57@gmail.com>
2024-05-04 18:27:13 -0500

    responsive x2 xd

--- components/Home/NewBanner.js
+++ components/Home/NewBanner.js
@@ -4,7 +4,7 @@ import { Button, Image, Input } from "@nextui-org/react";
 const NewBanner = () => {
   return (
     <div className="px-3 sm:flex items-center justify-center max-w-full py-10">
-      <div className="sm:flex items-center space-x-60 w-4/5">
+      <div className="sm:flex items-center space-x-6 w-full sm:w-4/5">
         <div>
           <div>
             <h1 className="Newbanner text-6xl font-extrabold sm:text-8xl ">
@@ -14,24 +14,25 @@ const NewBanner = () => {
               Programamos soluciones, creamos experiencias.
             </h3>
           </div>
-          <div className="pt-7 flex items-center">
+          <div className="pt-7 flex flex-col sm:flex-row items-center">
             <Input
               type="email"
               label="Email"
               variant="bordered"
-              className="h-12 mr-2"
+              className="h-12 mb-2 sm:mr-2 sm:mb-0"
             />
             <Button className="buton h-12" color="primary">
               Contactanos
             </Button>
           </div>
         </div>
-        <div className="max-sm:py-5 flex justify-start items-center">
+        <div className="hidden sm:flex max-sm:py-5 pl-40">
           <Image
             width={500} // Establece el ancho deseado en píxeles
             height={500} // Establece la altura deseada en píxeles
             alt="NextUI Fruit Image with Zoom"
             src="assets/img/logo.png"
+            className="mx-auto"
           />
         </div>
       </div>
