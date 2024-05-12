cde299382edcca37a9bfa862e67e4eaababb93a4
Santiago Ramirez Forero <santiforero57@gmail.com>
2024-05-12 13:29:50 -0500

    alt imagenes SEO

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import NewContactBanner from "@/components/Home/NewContactBanner";
 import NewBanner from "@/components/Home/NewBanner";
 import NewProyects from "@/components/Home/Proyects/NewProyects";
@@ -5,9 +6,9 @@ import Team from "@/components/Home/Team/Team";
 import ContactUs from "@/components/Home/ContactUs/ContactUs";
 import Clientes from "@/components/Home/Clientes/Clientes";
 import WhatsAppButton from "@/components/Home/WhatsAppButton/WhatsAppButton";
-
 import AboutSectionWithCards from "@/components/Home/AboutUs/AboutSectionWithCards";
 import AboutSection from "@/components/Home/AboutUs/AboutSection";
+
 export default function Home() {
   return (
     <>
