80c5223171305b7f125fc8e65e41657cd6623842
Santiago Ramirez Forero <santiforero57@gmail.com>
2024-05-13 17:09:19 -0500

    lenguajes no aparecen en celular

--- components/Home/AboutUs/AboutSection.js
+++ components/Home/AboutUs/AboutSection.js
@@ -8,6 +8,10 @@ import "swiper/css";
 import "swiper/css/effect-cards";
 import { EffectCards } from "swiper/modules";
 
+const cardClasses = "mb-8 lg:mb-0 lg:w-1/3";
+const slideClasses =
+  "flex items-center justify-center bg-white rounded-2xl text-base font-bold shadow-lg";
+
 function AboutSection({ slides }) {
   const swiperRef = useRef(null);
 
@@ -34,7 +38,7 @@ function AboutSection({ slides }) {
             Desarrollo Web por Sibartech
           </h1>
           <div className="flex flex-col lg:flex-row items-center py-8">
-            <div className="mb-8 lg:mb-0 lg:w-1/3 ">
+            <div className={cardClasses}>
               <CardComponent
                 title="Sobre Nosotros"
                 subtitle="Transformando Ideas en Realidad"
@@ -42,77 +46,31 @@ function AboutSection({ slides }) {
                 footer={<a href="#contacto">Puedes contactarnos</a>}
               />
             </div>
-            <div className="mb-8 lg:mb-0 lg:w-1/3">
+            <div className={cardClasses}>
               <Accordion variant="splitted">
-                <AccordionItem
-                  data-open="true"
-                  aria-label="¿Qué Hacemos?"
-                  title="¿Qué Hacemos?"
-                >
-                  <p>
-                    Desde el corazón de la programación, creamos soluciones que
-                    impulsan el progreso. En Sibartech, no solo construimos
-                    líneas de código, construimos puentes digitales entre
-                    conceptos y ejecución. Nos especializamos en el desarrollo
-                    de proyectos a medida que abarcan desde aplicaciones web y
-                    móviles hasta sistemas complejos de gestión. Cada proyecto
-                    es una oportunidad para demostrar nuestra destreza técnica y
-                    creativa.
-                  </p>
-                </AccordionItem>
-                <AccordionItem
-                  aria-label="Comprometidos con tu Éxito"
-                  title="Comprometidos con tu Éxito"
-                >
-                  <p>
-                    En Sibartech, entendemos que el desarrollo de software no
-                    termina con la entrega del proyecto. Nos comprometemos a
-                    brindar un soporte excepcional que garantice el rendimiento
-                    continuo y la adaptabilidad de nuestras soluciones. Nuestro
-                    equipo de expertos está siempre disponible para abordar
-                    cualquier desafío y garantizar que tu sistema funcione sin
-                    problemas, permitiéndote centrarte en lo que haces mejor:
-                    hacer crecer tu negocio.
-                  </p>
-                </AccordionItem>
-                <AccordionItem
-                  aria-label="Nuestra Filosofía"
-                  title="Nuestra Filosofía"
-                >
-                  <p>
-                    En el mundo dinámico de la tecnología, la innovación es
-                    clave. En Sibartech, abrazamos la filosofía de la mejora
-                    continua. Mantenemos nuestros conocimientos actualizados con
-                    las últimas tendencias y tecnologías para ofrecerte
-                    soluciones vanguardistas que te posicionen a la vanguardia
-                    de tu industria.
-                  </p>
-                </AccordionItem>
+                {accordionItems.map((item, index) => (
+                  <AccordionItem key={index} {...item} />
+                ))}
               </Accordion>
             </div>
-            <div className="mb-8 lg:mb-0 lg:w-1/3">
+            <div className={`${cardClasses} hidden lg:block`}>
+              {/* ^^^ Añade la clase 'hidden lg:block' para ocultar en dispositivos móviles */}
               <Swiper
                 effect={"cards"}
                 grabCursor={true}
                 modules={[EffectCards]}
                 className="w-60 h-80 mx-auto"
                 loop={true}
-                autoplay={{
-                  delay: 2000,
-                  disableOnInteraction: false,
-                }}
+                autoplay={{ delay: 2000, disableOnInteraction: false }}
                 ref={swiperRef}
               >
                 {slides &&
                   slides.map((slide, index) => (
-                    <SwiperSlide
-                      key={index}
-                      className="flex items-center justify-center bg-white rounded-2xl text-base font-bold shadow-lg"
-                    >
+                    <SwiperSlide key={index} className={slideClasses}>
                       <div>
                         <img
                           width={600}
-                          height={50} // Ajusta la altura según sea necesario
+                          height={50}
                           alt="Desarrollo Web por Sibartech"
                           src={slide.src}
                         />
@@ -127,5 +85,52 @@ function AboutSection({ slides }) {
     </>
   );
 }
+
+const accordionItems = [
+  {
+    "data-open": true,
+    "aria-label": "¿Qué Hacemos?",
+    title: "¿Qué Hacemos?",
+    children: (
+      <p>
+        Desde el corazón de la programación, creamos soluciones que impulsan el
+        progreso. En Sibartech, no solo construimos líneas de código,
+        construimos puentes digitales entre conceptos y ejecución. Nos
+        especializamos en el desarrollo de proyectos a medida que abarcan desde
+        aplicaciones web y móviles hasta sistemas complejos de gestión. Cada
+        proyecto es una oportunidad para demostrar nuestra destreza técnica y
+        creativa.
+      </p>
+    ),
+  },
+  {
+    "aria-label": "Comprometidos con tu Éxito",
+    title: "Comprometidos con tu Éxito",
+    children: (
+      <p>
+        En Sibartech, entendemos que el desarrollo de software no termina con la
+        entrega del proyecto. Nos comprometemos a brindar un soporte excepcional
+        que garantice el rendimiento continuo y la adaptabilidad de nuestras
+        soluciones. Nuestro equipo de expertos está siempre disponible para
+        abordar cualquier desafío y garantizar que tu sistema funcione sin
+        problemas, permitiéndote centrarte en lo que haces mejor: hacer crecer
+        tu negocio.
+      </p>
+    ),
+  },
+  {
+    "aria-label": "Nuestra Filosofía",
+    title: "Nuestra Filosofía",
+    children: (
+      <p>
+        En el mundo dinámico de la tecnología, la innovación es clave. En
+        Sibartech, abrazamos la filosofía de la mejora continua. Mantenemos
+        nuestros conocimientos actualizados con las últimas tendencias y
+        tecnologías para ofrecerte soluciones vanguardistas que te posicionen a
+        la vanguardia de tu industria.
+      </p>
+    ),
+  },
+];
 
 export default AboutSection;
